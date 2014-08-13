eaa4ff25f7265313ad7bc8ce2b4a92b9bd60a7ea
Jon Barker <jonathan.t.barker@gmail.com>
2014-08-12 23:17:46 -0700

    guessing game

---
+++ guessing.cpp
@@ -0,0 +1,27 @@
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+  // Generate unique random numbers using the current time
+  srand(time(NULL));
+  // Get a random number between 0 and 99
+  unsigned int numberToGuess = rand() % 100;
+ 
+  cout << "Guess a number between 0 and 99" << endl;
+ 
+  unsigned int playersNumber {};
+  cin >> playersNumber;
+ 
+  cout << "You guessed: "
+       << playersNumber
+       << " The actual number was: "
+       << numberToGuess
+       << endl;
+ 
+  return 0;
+}
